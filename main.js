1130240b46316dff3a84f80091adb9409181490f
Kiks <kevin.kiks.richard@gmail.com>
2022-08-24 14:03:46 +0200

    INITIAL COMMIT

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+function average(a, b, c) {
+    return (a + b) / 2 + c;
+}
+;
+console.log(average(100, 200, 300));
---
+++ main.ts
@@ -0,0 +1,7 @@
+console.log("Hello World");
+
+function average (a: number, b: number, c: number): number {
+    return (a + b) / 2 + c;
+};
+
+console.log(average(100, 200, 300));
